7bf189398861fdca6ddbac132d98a1e264b90ace
Cole Maring <65455664+colemaring@users.noreply.github.com>
2023-11-09 11:56:48 -0500

    wrote routes for crud of dump object list
    get the entire list for a specific user on login, create a new empty list for a new user on signup, append a new object to the end of the list for an existing user, remove an object at a certain index for an existing user, and update an object at a certain index for an existing user.

--- backend/models/data.js
+++ backend/models/data.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
-const dataSchema = new mongoose.Schema({
+const itemSchema = new mongoose.Schema({
   title: {
     type: String,
   },
   text: {
     type: String,
   },
+});
+
+const dataSchema = new mongoose.Schema({
+  items: [itemSchema],
 });
 
 module.exports = mongoose.model("Data", dataSchema);
--- backend/routes/data.js
+++ backend/routes/data.js
@@ -2,25 +2,75 @@ const express = require("express");
 const router = express.Router();
 const Data = require("../models/data");
 
-// Get the list
-router.get("/", async (req, res) => {
+// Get the list, for a specific user
+// used when user logs in
+router.get("/:id", async (req, res) => {
   try {
-    const data = await Data.find();
+    const data = await Data.findById(req.params.id);
     res.json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-// Create a new list
-router.post("/", async (req, res) => {
-  const data = new Data({
-    title: req.body.title,
-    text: req.body.text,
-  });
+// Create a new list, only for when new user is made
+// used for user account creation
+router.post("/:id", async (req, res) => {
+  // create a new list
+  const data = new Data({});
   try {
     const newData = await data.save();
     res.status(201).json(newData);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Append a new object to the array for a specific user
+// used for the create button
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedData = await Data.findByIdAndUpdate(
+      req.params.id,
+      { $push: { items: req.body } },
+      { new: true }
+    );
+    res.json(updatedData);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Remove an object at a certain index in the list for a specific user
+router.put("/:id/removeAtIndex/:index", async (req, res) => {
+  try {
+    const index = req.params.index;
+    let unset = {};
+    unset[`items.${index}`] = 1;
+    await Data.findByIdAndUpdate(req.params.id, { $unset: unset });
+    const updatedData = await Data.findByIdAndUpdate(
+      req.params.id,
+      { $pull: { items: null } },
+      { new: true }
+    );
+    res.json(updatedData);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Update an object at a certain index in the list for a specific user
+router.put("/:id/:index", async (req, res) => {
+  try {
+    const index = req.params.index;
+    let set = {};
+    set[`items.${index}`] = req.body;
+    const updatedData = await Data.findByIdAndUpdate(
+      req.params.id,
+      { $set: set },
+      { new: true }
+    );
+    res.json(updatedData);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
