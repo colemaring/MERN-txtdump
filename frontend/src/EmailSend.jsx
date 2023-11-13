f5df1d9810fc41d078c609109f9970e3d6917c74
lukeMaggio24 <139585492+lukeMaggio24@users.noreply.github.com>
2023-11-13 15:42:08 -0500

    made sensitive data more secure

--- frontend/src/EmailSend.jsx
+++ frontend/src/EmailSend.jsx
@@ -2,27 +2,24 @@ import SignUp from "./SignUp";
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 
-
-
 const nodemailer = require("nodemailer");
-
 
 export default function EmailSend() {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: "smtp-relay.brevo.com",
-        port: 587,
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
         secure: false, // true for 465, false for other ports
         auth: {
-        user: "txtdump563@gmail.com", // generated brevo user
-        pass: "Chdgr98NWmFZf0KJ", // generated brevo password
+        user: process.env.SMTP_USER, // generated brevo user
+        pass: process.env.SMTP_PASS, // generated brevo password
         },
   });
 
   // send mail with defined transport object
     let info =  transporter.sendMail({
-        from: '"txtdump563@gmail.com', // sender address
-        to: "john@domain.com", // list of receivers
+        from: process.env.SMTP_HOST, // sender address
+        to: "john@domain.com", // list of receivers, should be dynamic
         subject: "Confirm Txt Dump Account", // Subject line
         text: "Hello" + username +" , Thank you for signing up to our txt dump app", // plain text body
     });
