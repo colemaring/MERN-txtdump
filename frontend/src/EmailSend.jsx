09d428fda58619c5b08961dd23d149a5489a2a1d
lukeMaggio24 <139585492+lukeMaggio24@users.noreply.github.com>
2023-11-13 15:57:25 -0500

    Revert "Create EmailSend.jsx"
    This reverts commit 9da4df47e76970d76dd8ab98c8d91e2dca44e565.

--- frontend/src/EmailSend.jsx
+++ frontend/src/EmailSend.jsx
@@ -2,24 +2,27 @@ import SignUp from "./SignUp";
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 
+
+
 const nodemailer = require("nodemailer");
+
 
 export default function EmailSend() {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
+        host: "smtp-relay.brevo.com",
+        port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
-        user: process.env.SMTP_USER, // generated brevo user
-        pass: process.env.SMTP_PASS, // generated brevo password
+        user: "txtdump563@gmail.com", // generated brevo user
+        pass: "Chdgr98NWmFZf0KJ", // generated brevo password
         },
   });
 
   // send mail with defined transport object
     let info =  transporter.sendMail({
-        from: process.env.SMTP_HOST, // sender address
-        to: "john@domain.com", // list of receivers, should be dynamic
+        from: '"txtdump563@gmail.com', // sender address
+        to: "john@domain.com", // list of receivers
         subject: "Confirm Txt Dump Account", // Subject line
         text: "Hello" + username +" , Thank you for signing up to our txt dump app", // plain text body
     });
