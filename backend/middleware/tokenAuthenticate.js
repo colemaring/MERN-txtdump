2bf7136c21595c6242f3f3d3d21035a37e5cf7b3
KyleCostaWensloff <>
2023-11-26 13:57:45 -0500

    updated tokenAuthenticate.js

--- backend/middleware/tokenAuthenticate.js
+++ backend/middleware/tokenAuthenticate.js
@@ -1,19 +1,23 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+// middleware to authenticate JWT tokens in requests
 const authenticateJWTToken = (request, response, next) => {
+  // this will extract the authorization header form any incoming request
   const authHeader = request.headers.authorization;
 
   if (authHeader) {
     const token = authHeader.split(" ")[1];
     const secretKey = process.env.JWT_SECRET;
 
+    // verifies if the token is using the secret key
     jwt.verify(token, secretKey, (error, decodedUser) => {
       if (error) {
         console.error(error);
         return response.sendStatus(403); // Forbidden
       }
 
+      // attach the decoded user info to the request then proceed to next middleware
       request.user = decodedUser;
       console.log("user authenticated");
       next();
